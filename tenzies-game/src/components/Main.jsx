8a48935234b21009ce46cc98a882d98a2fd2d9d2
Daivion Brooks <daivionbrooks11@gmail.com>
2025-11-01 21:46:21 -0400

    vite template for project

---
+++ tenzies-game/src/components/Main.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function Main() {
+
+
+  return (
+    <>
+        <header>Tenzies</header>
+        <p>Roll until dice are the same. Click each die to freeze it at its current value between rolls.</p>
+    </>
+  )
+}
